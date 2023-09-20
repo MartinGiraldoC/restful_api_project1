24737a46bdc5f2f8d543183f1da7a74b1ee2a4a9
Martin Giraldo Ceballos <ceballosd990@gmail.com>
2023-09-20 17:04:13 -0500

    Erorr

--- services/userService.js
+++ services/userService.js
@@ -30,9 +30,25 @@ const createUser = async (name,email,phone,password)=>{
         throw {status:500, message:'User could not be created'}
     }
 }
+const updateUser = async (id, name, email, phone, password) => {
+    try {
+        let upNewUser = await db.User.update({
+            name, email, phone, password
+        },{
+            where: {
+                id,
+            }
+        })
+
+        return upNewUser
+        
+    } catch (error) {
+        return error.message || 'Falied to update user'
+    }
+}
 
 module.exports = {
     getAllUsers,
     getUser,
     createUser
-}
+};
